a1a01980e3d8f9202fe9499d104a901c6802fb17
jrbooth3 <jrbooth3@gmail.com>
2021-03-22 19:09:49 +0000

    03_22_21_19:03

--- www/test2.php
+++ www/test2.php
@@ -5,7 +5,7 @@
  <body>
 <?php
 echo "<body style='background-color:black;'>";
-echo "<font size='18'>";
+echo "<font size='120px'>";
 echo "<font color='lightgreen'>";
 function convertToHoursMins($time, $format = '%02d:%02d') {
     if ($time < 1) {
@@ -35,36 +35,11 @@ $result36hrows = pg_num_rows($result36h);
 $sql60s = "select distinct  name, status FROM svrstatus where date >= (NOW() - INTERVAL '60 second' ) AND ( status != '' ) order by name ;";
 $result60s = pg_query($sql60s);
 echo "<table border=1";
-//if ( $result36hrows < '1' ) {
-//echo "no status changes";
-//}
-/*
-echo "<th colspan='2'>Device Change Status</th>";
-echo "<tr style='color:lightgreen'> <th> Device </th><th colspan='2'>DateTime</th> <th> Status</th><th>uptime</th></tr>";
-if ( $result36hrows < '1' ) {
-echo "<tr style='color:lightgreen'><td colspan = '5'> no device status changes </td></tr>";
-}
-while($myrow = pg_fetch_assoc($result36h)) { 
-$newdate = $myrow['cleandate'];
-$interval = (strtotime($newdate) - strtotime($prevdate)) / 60;
-$uptime = convertToHoursMins($interval, '%02d h %02d m');
-//echo "previous date was $prevdate up time $interval - $uptime <br>";
-echo "<tr style='color:lightgreen'><td>" . $myrow['name']  . "</td><td colspan='2'>" . $myrow['cleandate'] . "</td><td>" . $myrow['status'] . "</td><td>";
-$uptest = $myrow['status'];
-//if ($uptest == "down" && $interval < "500" && $interval > "10") {
-if ($uptest == "down") {
-echo $uptime;
-} else {
-echo "";
-}
-echo "</td></tr>"; 
-$prevdate = $myrow['cleandate'];
-        }
-*/
-echo "<th colspan='5' style='color:lightgreen'>Device current Status</th>";
-echo "<tr style='color:lightgreen'><th colspan='3'> Device </th><th colspan='2'> Status</th></tr>";
+
+echo "<th colspan='2' style='color:lightgreen; font-size:56px'>Device current Status</th>";
+echo "<tr style='color:lightgreen'; font-size:56px><th colspan='2'> Device </th><th colspan='2'> Status</th></tr>";
 while($myrow = pg_fetch_assoc($result60s)) { 
-echo "<tr style='color:lightgreen'><td colspan='3'>".$myrow['name'] . "</td><td colspan='2'>". $myrow['status'] . "</td></tr>"; 
+echo "<tr style='color:lightgreen; font-size:46px'><td colspan='3'>".$myrow['name'] . "</td><td colspan='2'>". $myrow['status'] . "</td></tr>"; 
         }
 echo "</table>"; 
 ?>
