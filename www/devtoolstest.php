93a5b857fb0f8482e227d01fff3cde32611eb40c
jrbooth3 <jrbooth3@gmail.com>
2021-04-21 08:34:56 -0500

    04_21_21_08:04

--- www/devtoolstest.php
+++ www/devtoolstest.php
@@ -39,90 +39,92 @@ Network<br><br>
 Topology<br>
 IP address Lethe001/X0=10.0.0.1<br>
 IP address Lethe001/X1=dynamic from router<br>
+
+IP address Lethe001/X2=192.168.86.4<br><br>
+IP address Styx0001/X0=192.168.86.3<br>
+IP address Styx0001/X1=dynamic router<br><br>
+IP address Google wifi (acheron)192.168.86.1 <br>
+IP address Google internet 10.0.0.2<br>
+IP address Netgear NightHawk <br><br>
+router > switch1/vlan1 > TOR wifi router advertised name Lethe PorkPie321<br>
+router > switch1/vlan1 > Lethe001/X1 > Lethe001/X0 > Google mesh internet<br>
+router > switch1/vlan1 > Lethe001/X1 > Lethe001/X2 > switch1/vlan0 for port forwarding<br>
+router > switch2/vlan1 > Styx0001/X1 > Styx0001/X0 > switch2/vlan0<br><br>
+vlan0 switch ports 9-47 internal network<br>
+vlan1 switch ports 1-8,48 internet facing<br><br>
+Google mesh wifi network on switch1 vlan0<br>
+Google mesh internet network Lethe001/X0<br>
+Netgear NightHawk wifi network on switch2 vlan0<br><br>
+Switches<br>
+---Cisco<br>
+------2960G 10/100/1000 48 port<br>
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionsw1()'>Switch1</button>
+<script>
+function myFunctionsw1() {
+window.open('http://switch1')
+;}
+echo "</script><br>
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionsw2()'>Switch2</button>
+<script>
+function myFunctionsw2() {
+window.open('http://switch2')
+;}
+</script><br><br>
+firewalls<br>
+---SonicWall<br>
+------PRO 5060 10/100/1000 primary<br>
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionfw2()'>Lethe001</button>
+<script>
+function myFunctionfw2() {
+window.open('https://lethe001')
+;}
+e</script><br>
+
+------PRO 2040 10/100 secondary<br>
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionfw1()'>Styx0001</button>
+<script>
+function myFunctionfw1() {
+window.open('https://styx0001')
+;}
+</script><br><br>
+
+--Systems--<br>
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionnet()'>System's status pages</button>
+<script>
+function myFunctionnet() {
+indow.open('serverstatus.php')
+;}
+</script><br><br>
+--Development sites--<br>
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctiondockerhub()'>Docker Hub</button>
+<script>
+function myFunctiondockerhub() {
+window.open('https://hub.docker.com/')
+;}
+</script><br>
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctiongit()'>GIT</button>
+<script>
+function myFunctiongit() {
+window.open('https://github.com/jrbooth3')
+;}
+</script><br>
 <?php
-echo "IP address Lethe001/X2=192.168.86.4<br><br>";
-echo "IP address Styx0001/X0=192.168.86.3<br>";
-echo "IP address Styx0001/X1=dynamic router<br><br>";
-echo "IP address Google wifi (acheron)192.168.86.1 <br>";
-echo "IP address Google internet 10.0.0.2<br>";
-echo "IP address Netgear NightHawk <br><br>";
-echo "router > switch1/vlan1 > TOR wifi router advertised name Lethe PorkPie321<br>";
-echo "router > switch1/vlan1 > Lethe001/X1 > Lethe001/X0 > Google mesh internet<br>";
-echo "router > switch1/vlan1 > Lethe001/X1 > Lethe001/X2 > switch1/vlan0 for port forwarding<br>";
-echo "router > switch2/vlan1 > Styx0001/X1 > Styx0001/X0 > switch2/vlan0<br><br>";
-echo "vlan0 switch ports 9-47 internal network<br>";
-echo "vlan1 switch ports 1-8,48 internet facing<br><br>";
-echo "Google mesh wifi network on switch1 vlan0<br>";
-echo "Google mesh internet network Lethe001/X0<br>";
-echo "Netgear NightHawk wifi network on switch2 vlan0<br><br>";
-echo "Switches<br>";
-echo "---Cisco<br>";
-echo "------2960G 10/100/1000 48 port<br>";
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionsw1()'>Switch1</button>";
-echo "<script>";
-echo "function myFunctionsw1() {";
-echo "window.open('http://switch1')";
-echo ";}";
-echo "</script><br>";
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionsw2()'>Switch2</button>";
-echo "<script>";
-echo "function myFunctionsw2() {";
-echo "window.open('http://switch2')";
-echo ";}";
-echo "</script><br><br>";
-echo "firewalls<br>";
-echo "---SonicWall<br>";
-echo "------PRO 5060 10/100/1000 primary<br>";
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionfw2()'>Lethe001</button>";
-echo "<script>";
-echo "function myFunctionfw2() {";
-echo "window.open('https://lethe001')";
-echo ";}";
-echo "</script><br>";
-
-echo "------PRO 2040 10/100 secondary<br>";
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionfw1()'>Styx0001</button>";
-echo "<script>";
-echo "function myFunctionfw1() {";
-echo "window.open('https://styx0001')";
-echo ";}";
-echo "</script><br><br>";
-
-echo "--Systems--<br>";
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionnet()'>System's status pages</button>";
-echo "<script>";
-echo "function myFunctionnet() {";
-echo "window.open('serverstatus.php')";
-echo ";}";
-echo "</script><br><br>";
-echo "--Development sites--<br>";
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctiondockerhub()'>Docker Hub</button>";
-echo "<script>";
-echo "function myFunctiondockerhub() {";
-echo "window.open('https://hub.docker.com/')";
-echo ";}";
-echo "</script><br>";
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctiongit()'>GIT</button>";
-echo "<script>";
-echo "function myFunctiongit() {";
-echo "window.open('https://github.com/jrbooth3')";
-echo ";}";
-echo "</script><br>";
 if ( $test == "1" ) {
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionDe()'>Call Jenkins to deploy Code</button>";
-echo "<script>";
-echo "function myFunctionDe() {";
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctionDe()'>Call Jenkins to deploy Code</button>
+<script>
+function myFunctionDe() {
+
 echo shell_exec("curl -X POST http://auto:11c2b82cd6caa3c9a53036d127ce5064c9@192.168.86.27:8083/job/dockerPreBuild/build?token=SKLDjflekjrtlk4j3lkjslkj4lk3PEKDG");
-echo "window.open('auto.php', 'target=_blank')";
-echo "window.open('http://artemis:8083/job/dbNoderefresh/')";
-echo "</script><br>";
+echo "window.open('auto.php', 'target=_blank')
+echo "window.open('http://artemis:8083/job/dbNoderefresh/')
+echo "</script><br>
 } else {
-echo "<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctiontestsite()'>Test site</button>";
-echo "<script>";
-echo "function myFunctiontestsite() {";
-echo "window.open('http://artemis:81/')";
-echo ";}";
-echo "</script><br>";
+<button style='font-size : 36px; background-color:lightgreen' onclick='myFunctiontestsite()'>Test site</button>
+<script>
+function myFunctiontestsite() {
+window.open('http://artemis:81/')
+;}
+</script><br>
 }
 ?>
 <script type="text/javascript">
